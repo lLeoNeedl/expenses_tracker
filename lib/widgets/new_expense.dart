a49358b6247730c9e695b7ddda4f85473831511f
Leonid Lavrov <274852@gmail.com>
2023-09-17 15:58:03 +0300

    Working with text input

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleContoller = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleContoller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleContoller,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleContoller.text);
+                },
+                child: const Text('Save expense'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
